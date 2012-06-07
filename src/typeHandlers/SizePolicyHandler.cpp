fb6403cf64dfa7f81db1ffc4a54c3d3d54a54ee2
Manuel Reithuber <git@manuel.reithuber.net>
2012-06-07 16:10:35 +0200

    removed obsolete code from SizePolicyHandler

--- src/typeHandlers/SizePolicyHandler.cpp
+++ src/typeHandlers/SizePolicyHandler.cpp
@@ -21,37 +21,9 @@ QString SizePolicyHandler::toString(const QVariant &variant) {
 }
 
 QString SizePolicyHandler::toString(QSizePolicy::Policy policy) {
-#if 0
-	QString rc = "UNKNOWN";
-	switch (policy) {
-	case QSizePolicy::Fixed:
-		rc = "Fixed";
-		break;
-	case QSizePolicy::Minimum:
-		rc = "Minimum";
-		break;
-	case QSizePolicy::Maximum:
-		rc = "Maximum";
-		break;
-	case QSizePolicy::Preferred:
-		rc = "Preferred";
-		break;
-	case QSizePolicy::MinimumExpanding:
-		rc = "MinimumExpanding";
-		break;
-	case QSizePolicy::Expanding:
-		rc = "Expanding";
-		break;
-	case QSizePolicy::Ignored:
-		rc = "Ignored";
-		break;
-	}
-	return rc;
-#else
 	const QMetaObject &metaObject = QSizePolicy::staticMetaObject;
 	int index = metaObject.indexOfEnumerator("Policy");
 	QMetaEnum enumerator = metaObject.enumerator(index);
 
 	return enumerator.valueToKey(policy);
-#endif
 }
